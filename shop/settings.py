8e373ac967dbb1f4ef2e3072b4e2f64dbd202439
FER <egorlymar1@gmail.com>
2025-07-18 22:19:11 +0300

    wrote the cart, made the processor context

--- shop/settings.py
+++ shop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "main",
+    "cart",
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "cart.context_processors.cart"
             ],
         },
     },
@@ -127,3 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CART_SESSION_ID = "cart"
